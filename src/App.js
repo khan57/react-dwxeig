471d0067085cb067f676a409431f879e6fa7229e
khan57 <haseeb.ktk57@gmail.com>
2022-02-12 10:53:05 +0500

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
-import "./style.css";
+import React, { useEffect, useState } from 'react';
+import './global.css';
 
 export default function App() {
+  const [chats, setChats] = useState([]);
+  const getMyChats = () => {
+    var myHeaders = new Headers();
+    myHeaders.append(
+      'Authorization',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjFmZmM2Nzk0YWUwYjA4MWQ1OGNiMGRlIiwiaWF0IjoxNjQ0MzMyNjgxfQ.7GgW7t4uIRwCWf8kFyUM6e9WTv6Ff6Jav5ulVOsGuUQ'
+    );
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+    };
+
+    fetch('http://localhost:3000/api/v1/conversation/', requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result.data);
+        setChats(result.data);
+      })
+      .catch((error) => console.log('error', error));
+  };
+
+  useEffect(() => {
+    getMyChats();
+  }, []);
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="app">
+      <div className="container">
+        <div style={{ flexGrow: 1 }}>
+          {chats.map((chat) => (
+            <div style={{ marginBottom: 4 }}>{chat.channel_name}</div>
+          ))}
+        </div>
+        <div className="message-body">sakdjas</div>
+      </div>
     </div>
   );
 }
